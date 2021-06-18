82b35809f8aa980a5b7028f719884d6728f8e2a0
Rolando Andre <rbardalespriale@gmail.com>
2021-06-18 01:28:39 -0500

    fix : cambios flatMap
    Se modifico la clase para dejar un ejemplo carlo de lo que es el operador flatMap en Stream.

--- src/org/aguzman/api/stream/ejemplos/EjemploStreamFlatMapFactura.java
+++ src/org/aguzman/api/stream/ejemplos/EjemploStreamFlatMapFactura.java
@@ -1,6 +1,5 @@
 package org.aguzman.api.stream.ejemplos;
 
-import org.aguzman.api.stream.ejemplos.models.Factura;
 import org.aguzman.api.stream.ejemplos.models.Usuario;
 
 import java.util.ArrayList;
@@ -12,24 +11,12 @@ public class EjemploStreamFlatMapFactura {
 
     public static void main(String... args) {
 
-        /*
-         El operador flatMap, permite retornar un Stream,
-         esto nos ayuda a por ejemplo en este caso bifurcar un Stream
-         de la clase Usuario en Stream de la clase Factura. De esta forma recorremos
-         todas las facturas de cada usuario.
-         */
         Usuario usuario = new Usuario("John", "Doe");
-        Usuario usuarioDos = new Usuario("Pepe", "Perez");
-
-        usuario.addFactura(new Factura("compras tecnologias"));
-        usuarioDos.addFactura(new Factura("bicicleta"));
-        usuarioDos.addFactura(new Factura("notebook gamer"));
+        Usuario usuarioDos = new Usuario("Mateo", "Luck");
 
         List<Usuario> usuarioList = new ArrayList<>(Arrays.asList(usuario, usuarioDos));
-
         Stream.ofNullable(usuarioList)
                 .flatMap(List::stream)
-                .flatMap(user -> user.getFacturas().stream())
                 .forEach(System.out::println);
 
     }
